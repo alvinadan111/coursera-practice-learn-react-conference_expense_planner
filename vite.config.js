fbbeb05882d58b7c2001f285171c92078921d408
alvinadan <alvin.adan.111@gmail.com>
2025-06-13 09:21:16 +0800

    added base prop

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/coursera-practice-learn-react-conference_expense_planner/", //name of github repo
   plugins: [react()],
-})
+});
